28d7d2eeb7b9339e716394a5817548d9dcc08927
Sebastian Sandqvist <sandquist.sebastian@gmail.com>
2018-11-01 13:34:36 +0100

    added media querys for seriously tiny devices

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
   render() {
     return (
     <div className="footer-content-wrapper">
-      <div className="row">
+      <div className="row footer-row">
         <div className="col">
           <h5>Kontakt</h5>
           <p>
@@ -19,9 +19,11 @@ class Footer extends Component {
           <h5>Sociala Medier</h5>
           <p><a href="#"><i className="fab fa-facebook"></i> </a> &emsp; <a href="#"><i className="fab fa-instagram"></i></a>  </p>
         </div>
-        <p>
-          <i className="fas fa-home"></i> &emsp; Karlsbodavägen 2,168 67 Bromma
-        </p>
+        <div className="col">
+          <p>
+            <i className="fas fa-home"></i> &emsp; Karlsbodavägen 2,168 67 Bromma
+          </p>
+        </div>
       </div>
     </div>
 
